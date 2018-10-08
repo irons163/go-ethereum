ef7037e5630ca02519dcf86967bd946f7c3f4ace
Phil.Chang <wen-chuan.chang@senao.com>
2018-10-08 17:33:03 +0800

    Comments

--- big.go
+++ big.go
@@ -2,13 +2,19 @@ package main
 
 import "math/big"
 
-func BigPow(a, b int) *big.Int {
-	c := new(big.Int)
-	c.Exp(big.NewInt(int64(a)), big.NewInt(int64(b)), big.NewInt(0))
+/*
+ * Returns the power of two integers
+ */
+func BigPow(a,b int) *big.Int {
+  c := new(big.Int)
+  c.Exp(big.NewInt(int64(a)), big.NewInt(int64(b)), big.NewInt(0))
 
 	return c
 }
 
+/*
+ * Like big.NewInt(uint64); this takes a string instead.
+ */
 func Big(num string) *big.Int {
 	n := new(big.Int)
 	n.SetString(num, 0)

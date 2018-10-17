854baf6c7033ed45dd6f8dc17fc3008cf5186ec7
Phil.Chang <wen-chuan.chang@senao.com>
2018-10-17 17:50:52 +0800

    Add trie.root test

--- trie_test.go
+++ trie_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"testing"
 )
 
@@ -28,10 +29,17 @@ func TestTrieUpdate(t *testing.T) {
 
 	trie.PutSatae([]byte("dog"), "puppy")
 	trie.PutSatae([]byte("dogglesworth"), "cat")
+	trie.PutSatae([]byte("doe"), "reindeer")
 
-	data := trie.Get([]byte(trie.root)) // 透過hash查找，得到root node
-	data = trie.Get([]byte(DecodeNode(data)[1])) // root node 有兩個欄位，key , value，取value欄位，branch node
-	data = trie.Get([]byte(DecodeNode(data)[6])) // expand node有26個欄位，對應字母a~z，取第7個欄位(index==6)，得到leaf node
-	PrintSliceReal(DecodeNode(data)) // 印出leaf node中的真實data:["7lesworth","cat"]
-	PrintSlice(DecodeNode(data)) // 印出易讀data：["glesworth","cat"]
+	//data := trie.Get([]byte(trie.root)) // 透過hash查找，得到root node
+	//data = trie.Get([]byte(DecodeNode(data)[1])) // root node 有兩個欄位，key , value，取value欄位，branch node
+	//data = trie.Get([]byte(DecodeNode(data)[6])) // expand node有26個欄位，對應字母a~z，取第7個欄位(index==6)，得到leaf node
+	//PrintSliceReal(DecodeNode(data)) // 印出leaf node中的真實data:["7lesworth","cat"]
+	//PrintSlice(DecodeNode(data)) // 印出易讀data：["glesworth","cat"]
+
+	root := hex.EncodeToString([]byte(trie.root))
+	req := "e378927bfc1bd4f01a2e8d9f59bd18db8a208bb493ac0b00f93ce51d4d2af76c"
+	if root != req {
+		t.Error("trie.root do not match, expected", req, "got", root)
+	}
 }
